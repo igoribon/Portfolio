96c3c52feebb4dda181cfd836c15ba093a08317b
igoribon <igoribon@gmail.com>
2025-05-11 19:14:35 +0200

    proyectos

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -9,6 +9,36 @@
         <div class="project-content">
           <div class="project-overline">Proyecto Destacado</div>
           <h3 class="project-title">
+            <a href="https://github.com/intxaurtietadev/meteobee" target="_blank" rel="noopener noreferrer">Meteobee</a>
+          </h3>
+          <div class="project-description">
+            <p>
+              Aplicación meteorológica que proporciona información climática detallada útil para los apicultores de Castilla y León, Extremadura y Andalucía. Integra datos en tiempo real de APIs de AEMET para ofrecer pronósticos
+              precisos y actualizados. Listado de ayudas y subvenciones para apicultores, así como un apartado de noticias relacionadas con el sector.
+              Permite a los usuarios registrarse y guardar sus colmenas, así como recibir notificaciones sobre el clima y las condiciones óptimas para la apicultura.
+            </p>
+          </div>
+          <ul class="project-tech-list">
+            <li>JavaScript</li>
+            <li>API Integration</li>
+            <li>Weather Data</li>
+          </ul>
+          <div class="project-links">
+            <a href="https://github.com/intxaurtietadev/meteobee" target="_blank" rel="noopener noreferrer" aria-label="GitHub Link">
+              <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+            </a>
+          </div>
+        </div>
+        <div class="project-image">
+          <img src="../assets/images/meteobee.jpg" alt="Screenshot de Meteobee" />
+        </div>
+      </article>
+
+
+      <article class="project-card">
+        <div class="project-content">
+          <div class="project-overline">Proyecto Destacado</div>
+          <h3 class="project-title">
             <a href="https://github.com/igoribon/fronted-Kidtopia" target="_blank" rel="noopener noreferrer">Kidtopia</a>
           </h3>
           <div class="project-description">
@@ -36,7 +66,6 @@
 
       <article class="project-card">
         <div class="project-content">
-          <div class="project-overline">Proyecto Destacado</div>
           <h3 class="project-title">
             <a href="https://github.com/TheGittersCodingMonsters/Foodify" target="_blank" rel="noopener noreferrer">Foodify</a>
           </h3>
@@ -62,38 +91,9 @@
         </div>
       </article>
 
-      <article class="project-card">
-        <div class="project-content">
-          <div class="project-overline">Proyecto Destacado</div>
-          <h3 class="project-title">
-            <a href="https://github.com/intxaurtietadev/meteobee" target="_blank" rel="noopener noreferrer">Meteobee</a>
-          </h3>
-          <div class="project-description">
-            <p>
-              Aplicación meteorológica que proporciona información climática detallada útil para los apicultores de Castilla y León, Extremadura y Andalucía. Integra datos en tiempo real de APIs de AEMET para ofrecer pronósticos
-              precisos y actualizados. Listado de ayudas y subvenciones para apicultores, así como un apartado de noticias relacionadas con el sector.
-              Permite a los usuarios registrarse y guardar sus colmenas, así como recibir notificaciones sobre el clima y las condiciones óptimas para la apicultura.
-            </p>
-          </div>
-          <ul class="project-tech-list">
-            <li>JavaScript</li>
-            <li>API Integration</li>
-            <li>Weather Data</li>
-          </ul>
-          <div class="project-links">
-            <a href="https://github.com/intxaurtietadev/meteobee" target="_blank" rel="noopener noreferrer" aria-label="GitHub Link">
-              <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
-            </a>
-          </div>
-        </div>
-        <div class="project-image">
-          <img src="../assets/images/meteobee.jpg" alt="Screenshot de Meteobee" />
-        </div>
-      </article>
 
       <article class="project-card">
         <div class="project-content">
-          <div class="project-overline">Proyecto Destacado</div>
           <h3 class="project-title">
             <a href="https://github.com/igoribon/Academy" target="_blank" rel="noopener noreferrer">Academia</a>
           </h3>
