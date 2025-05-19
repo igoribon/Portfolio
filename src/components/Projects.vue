cd1ee5bf0a7988e681a5c5ea401e0904c07e21b2
igoribon <igoribon@gmail.com>
2025-05-19 10:23:25 +0200

    proyecto examen

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -68,27 +68,26 @@
         <div class="project-content">
           <div class="project-overline"></div>
           <h3 class="project-title">
-            <a href="https://github.com/igoribon/Investigacion_vue" target="_blank" rel="noopener noreferrer">Investigación Vue</a>
+            <a href="https://igoribon.github.io/Igor-Aranguren---Prueba_tecnica/" target="_blank" rel="noopener noreferrer">Examen final Bootcamp</a>
           </h3>
           <div class="project-description">
             <p>
-              Investigación sobre el framework Vue.js. El objetivo es entender las ventajas y desventajas de cada uno. Incluye ejemplos prácticos como saludos, contadores, cambios de color y como final un Horóscopo personalizado. con cartas y recomendaciones.
+              Ejercicio práctico de final de Bootcamp. Aplicación para visitar un museo de arte. Permite a los usuarios registrarse, iniciar sesión y explorar las obras de arte disponibles por salas. 
             </p>
           </div>
           <ul class="project-tech-list">
             <li>HTML</li>
             <li>CSS</li>
             <li>Javascript</li>
-            <li>Vue</li>
           </ul>
           <div class="project-links">
-            <a href="https://github.com/igoribon/Investigacion_vue" target="_blank" rel="noopener noreferrer" aria-label="GitHub Link">
+            <a href="https://igoribon.github.io/Igor-Aranguren---Prueba_tecnica/" target="_blank" rel="noopener noreferrer" aria-label="GitHub Link">
               <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
             </a>
           </div>
         </div>
         <div class="project-image">
-          <img src="../assets/images/investigacion_vue.png" alt="Screenshot de Investigación Vue" />
+          <img src="../assets/images/examen.png" alt="Screenshot de examen" />
         </div>
       </article>
 
